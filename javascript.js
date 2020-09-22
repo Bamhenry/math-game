8a06d2aa47242daab809543586182d63acb04262
Brooke McHenry <mchenry.bam@gmail.com>
2020-09-22 12:56:48 -0500

    created countdown and game over display

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 var playing = false;
 var score;
+var action;
+var timeremaining;
 
 //if click on the start/reset button
 document.getElementById("startreset").onclick = function() {
@@ -20,10 +22,19 @@ document.getElementById("startreset").onclick = function() {
         document.getElementById("scorevalue").innerHTML = score;
 
         //show countdown box
-        document.getElementById("timeremaining").style.display = "block";
+        show("timeremaining");
+
+        timeremaining = 60;
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+
 
         //change button to reset game
         document.getElementById("startreset").innerHTML = "Reset Game";
+
+        //start countdown
+        startCountdown();
+
+
     }
 }
     //if playing
@@ -51,3 +62,36 @@ document.getElementById("startreset").onclick = function() {
             //no
                 //show try again box for 1 sec
 
+//functions
+
+//start counter
+function startCountdown(){
+    action = setInterval(function(){
+        timeremaining -= 1;
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+        if(timeremaining == 0){  //game over
+            stopCountdown();
+            show("gameOver");
+            document.getElementById("gameOver").innerHTML = "<p>Game Over!</p><p>Your score is " + score +".</p>";
+            hide("timeremaining");
+            hide("correct");
+            hide("wrong");
+            playing = false;
+        }
+    }, 1000);
+}
+
+//stop counter
+function stopCountdown(){
+    clearInterval(action);
+}
+
+//hide an element
+function hide(Id){
+    document.getElementById(Id).style.display = "none";
+}
+
+//show an element
+function show(Id){
+    document.getElementById(Id).style.display = "block";
+}
